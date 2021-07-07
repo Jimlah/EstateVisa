ba3409d6e1bb6317e23f578b6cb696db5f7b1c42
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-07 06:41:04 +0100

    Chore: Created AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $request['password'] = Hash::make($request['password']);
+        $request['remember_token'] = Str::random(10);
+        $user = User::create($request->toArray());
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+        return response($response, 200);
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $token = auth()->user()->createToken('Laravel Password Grant Client')->accessToken;
+            $response = ['token' => $token];
+            return response($response, 200);
+        }
+
+        $response = ["message" => 'Email or Password Invalid'];
+        return response($response, 422);
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink($request->only('email'));
+
+        return $status === Password::RESET_LINK_SENT
+        ? response()->json(['message' => 'Reset link sent to your email.', 'status' => true], 201)
+        : response()->json(['message' => 'Unable to send reset link', 'status' => false], 401);
+    }
+
+    public function passwordReset(Request $request)
+    {
+        return $request->query('token');
+    }
+}
