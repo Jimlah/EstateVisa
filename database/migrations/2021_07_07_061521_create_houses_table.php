020ce829ac82516da2cc5b65770e02a7bf0a81ef
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-07 13:39:59 +0100

    Create 2021_07_07_061521_create_houses_table.php

---
+++ database/migrations/2021_07_07_061521_create_houses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('house_types_id');
+            $table->string('code');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
