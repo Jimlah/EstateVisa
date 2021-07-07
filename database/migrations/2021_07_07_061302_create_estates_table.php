7a7a9d5d497b9230b0cb30b90cb2218f8f01eaee
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-07 13:36:07 +0100

    Created the estate module

---
+++ database/migrations/2021_07_07_061302_create_estates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('code')->unique();
+            $table->string('address')->nullable();
+            $table->string("logo")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estates');
+    }
+}
