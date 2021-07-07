288bcfcbed61fdb54c48689d246453659cea31a3
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-07 13:31:44 +0100

    Created the Account Creation Email Page

---
+++ resources/views/email/user/created.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Account Creation
+
+Hello User, thank you for signing up on the platform.
+
+Below is your Login details
+    Email: {{$user->email}}
+    Password: {{$password}}
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
