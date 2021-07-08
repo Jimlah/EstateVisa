c3c644a9b7308a20377fb1146b1d8b0515529d68
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-08 11:54:17 +0100

    removed the registration page from un-authenticated page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,24 +13,24 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+    // public function register(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         'email' => 'required|string|email|max:255|unique:users',
+    //         'password' => 'required|string|min:6|confirmed',
+    //     ]);
 
-        if ($validator->fails()) {
-            return response(['errors' => $validator->errors()->all()], 422);
-        }
+    //     if ($validator->fails()) {
+    //         return response(['errors' => $validator->errors()->all()], 422);
+    //     }
 
-        $request['password'] = Hash::make($request['password']);
-        $request['remember_token'] = Str::random(10);
-        $user = User::create($request->toArray());
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-        $response = ['token' => $token];
-        return response($response, 200);
-    }
+    //     $request['password'] = Hash::make($request['password']);
+    //     $request['remember_token'] = Str::random(10);
+    //     $user = User::create($request->toArray());
+    //     $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+    //     $response = ['token' => $token];
+    //     return response($response, 200);
+    // }
 
     public function login(UserLoginRequest $request)
     {
