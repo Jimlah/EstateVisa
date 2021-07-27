8db380b0ff7aebe93db66cc177dc097408a346c4
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-27 19:43:24 +0100

    Update ProfileResource.php

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -14,6 +14,13 @@ class ProfileResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'gender' => $this->gender,
+            'phone_number' => $this->phone_number,
+
+        ];
     }
 }
