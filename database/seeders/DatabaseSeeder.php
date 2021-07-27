d7eb65441f99844371658afac92c4dcda4cde463
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-07-27 19:43:58 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+
+        Profile::factory(10)->create();
 
         // $this->call([
         //     RolesTableSeeder::class,
