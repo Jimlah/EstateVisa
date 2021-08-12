769d47a2e63081823adbc86ae74951edfedcf49a
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-12 01:36:47 +0100

    Updated AuthTest

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Mail\UserCreated;
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 use Tests\TestCase;
@@ -21,31 +22,50 @@ class AuthTest extends TestCase
      *  */
     public function test_api_login()
     {
+        Artisan::call('migrate');
+        User::unsetEventDispatcher();
+        $user =  User::factory()->create();
+
         $body = [
-            'email' => "ggleichner@example.com",
+            'email' => $user->email,
             'password' => "password",
         ];
 
         $response = $this->json("POST", '/api/login', $body, ['Accept' => 'application/json']);
         $response->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>$json->has('token'));
-
+            ->assertJson(fn (AssertableJson $json) => $json->has('token'));
     }
 
+    public function test_api_will_not_login_with_invalid_data()
+    {
+        Artisan::call('migrate');
+        User::unsetEventDispatcher();
+        $user =  User::factory()->create();
+
+        $body = [
+            'email' => $user->email,
+            'password' => "pentacle",
+        ];
+
+        $response = $this->json("POST", '/api/login', $body, ['Accept' => 'application/json']);
+        $response->assertStatus(422);
+        $response->assertJson(fn (AssertableJson $json) => $json->has('message')->has('status'));
+        $response->dump();
+    }
 
 
     public function test_api_forgot_password()
     {
         $body = [
-          'email' => 'abdullahij951@gmail.com'
+            'email' => 'abdullahij951@gmail.com'
         ];
 
         $response = $this->json('POST', '/api/forgot-password', $body, ['Accept' => 'application/json']);
-        $response->assertJson(fn (AssertableJson $json) => $json->has('message')->has('status') );
+        $response->assertJson(fn (AssertableJson $json) => $json->has('message')->has('status'));
     }
 
 
-      /**
+    /**
      * @test api/auth/logout
      *
      * @return void
@@ -58,11 +78,12 @@ class AuthTest extends TestCase
         $response = $this->json("GET", '/api/logout', [], ['Accept' => 'application/json', "Authorization" => $this->bearer]);
         $response->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) =>
-        $json->has('message'));
+            $json->has('message'));
     }
 
     protected function login()
     {
+        User::factory()->create();
         $user = User::first();
         $token = $user->createToken('Application')->accessToken;
         $this->bearer = "Bearer $token";
