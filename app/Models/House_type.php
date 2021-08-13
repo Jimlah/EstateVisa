dea9f16819916e028005dacb4f67e759474789ba
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-13 15:49:04 +0100

    Create booted method for HouseType Controller

--- app/Models/House_type.php
+++ app/Models/House_type.php
@@ -2,12 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Estate;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class House_type extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'estate_id',
+        'name',
+        'description',
+        'code'
+    ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope(
+            'Estate_House_Type',
+            function (Builder $builder) {
+                if (auth()->user()->hasRole(User::ESTATE_OWNER)) {
+                    $builder->where(
+                        'estate_id',
+                        '=',
+                        auth()->user()->estates?->id
+                    );
+                }
+            }
+        );
+    }
+
+    public function estate()
+    {
+        return $this->belongsTo(Estate::class);
+    }
 
     public function house()
     {
