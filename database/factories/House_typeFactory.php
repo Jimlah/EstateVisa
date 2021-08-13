2300af2c81690693476e35b4f112045d1502850a
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-13 15:49:36 +0100

    Created factory for HouseType

---
+++ database/factories/House_typeFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\House_Type;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class House_typeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = House_Type::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return
+            [
+                'estate_id' => User::all()->random()->id,
+                'name' => $this->faker->word,
+                'description' => $this->faker->sentence,
+                'code' => $this->faker->word,
+            ];
+    }
+}
