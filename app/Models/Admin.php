59b23f7ef8e99fc799bb00ea567887289060c573
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-14 14:52:52 +0100

    Created admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Trait\UseDisable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Admin extends Model
+{
+    use HasFactory;
+    use UseDisable;
+
+
+    protected $fillable = [
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
