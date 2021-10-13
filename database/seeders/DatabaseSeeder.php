857d4f0adfe062882df31f74125547007c614433
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-10-13 13:20:42 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Admin;
 use App\Models\Estate;
-use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Database\Seeders\EstateHouseSeeder;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +18,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'email' => 'prohaska.jayde@example.net'
-        ]);
+        // User::factory()->create([
+        //     'email' => 'prohaska.jayde@example.net'
+        // ]);
 
         // Profile::factory()->create();
 
@@ -29,8 +30,35 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminSeeder::class,
-            EstateAdminSeeder::class,
             EstateSeeder::class,
+            EstateAdminSeeder::class,
+            HouseTypeSeeder::class,
+            EstateHouseSeeder::class,
         ]);
+        $this->command->info('Seeded: Admin, Estate, EstateAdmin, HouseType, EstateHouse');
+
+        User::all()->first()->update([
+            'email' => 'superadmin@admin.com',
+            'password' => bcrypt('password')
+        ]);
+        $this->command->info('Created Super Admin superadmin@admin.com');
+
+        Admin::find(1)->user()->update([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+        ]);
+        $this->command->info('Created Admin admin@admin.com');
+
+        Estate::find(1)->estate_admin()->first()->user()->update([
+            'email' => 'superadmin@estate.com',
+            'password' => bcrypt('password')
+        ]);
+        $this->command->info('Created Estate Super Admin superadmin@estate.com');
+
+        Estate::find(1)->estate_admin()->latest()->first()->user()->update([
+            'email' => 'admin@estate.com',
+            'password' => bcrypt('password')
+        ]);
+        $this->command->info('Created Estate Admin admin@estate.com');
     }
 }
