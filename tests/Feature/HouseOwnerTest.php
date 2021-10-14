869f4cf3934891ee3148e7c653d4b41be61559cd
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-10-14 21:58:38 +0100

    Create HouseOwnerTest.php

---
+++ tests/Feature/HouseOwnerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class HouseOwnerTest extends TestCase
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_house_owner_can_get_all_his_houses()
+    {
+        $response = $this->actingAs(static::$houseOwner, 'api')
+            ->getJson(route('house-owner.index'));
+
+        $response->assertStatus(200);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->has('data')
+                ->etc()
+        );
+    }
+
+    public function test_house_owner_can_view_a_single_house()
+    {
+        $response = $this->actingAs(static::$houseOwner, 'api')
+            ->getJson(route('house-owner.show', static::$houseOwner->houseOwner->first()->id));
+
+        $response->assertStatus(200);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->has('data')
+                ->etc()
+        );
+    }
+
+
+    public function test_estate_admin_can_add_a_user_to_a_house()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = array_merge(
+            User::factory()->make()->toArray(),
+            Profile::factory()->make()->toArray(),
+        );
+
+        $estateAdmin =  static::$estateAdmin;
+        $house = $estateAdmin->estate->random()->first()->houses->random()->first();
+
+        $response = $this->actingAs($estateAdmin, 'api')
+            ->putJson(route('house.update', $house->id), $attributes);
+
+        $response->assertStatus(200);
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json->has('message')
+                ->has('status')
+                ->etc()
+        );
+    }
+}
