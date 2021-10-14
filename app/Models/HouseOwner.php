98583814b606f49df8e741c501890a0e8c9d8337
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-10-14 21:58:02 +0100

    Update HouseOwner.php

--- app/Models/HouseOwner.php
+++ app/Models/HouseOwner.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class HouseOwner extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'estate_id',
+        'house_id',
+        'status'
+    ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('house_owner', function ($builder) {
+            if (auth()->check()) {
+                $builder->where('user_id', auth()->user()->id);
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function estate()
+    {
+        return $this->belongsTo(Estate::class, 'estate_id');
+    }
+
+    public function house()
+    {
+        return $this->belongsTo(House::class, 'house_id');
+    }
 }
