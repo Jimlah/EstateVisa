42cfdd602fbaabbf7b770d53bad1ae44f03c7c54
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-10-15 12:59:36 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,8 @@ class DatabaseSeeder extends Seeder
 
         // Profile::factory(10)->create();
 
+        User::unsetEventDispatcher();
+
         Artisan::call('passport:install');
         $this->command->info('Passport installed successfully.');
 
