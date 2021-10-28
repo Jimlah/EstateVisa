2b58428fdf0d278af1dd3ad2471524487be3103f
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-10-28 07:14:50 +0100

    Update GatePassController.php

--- app/Http/Controllers/GatePassController.php
+++ app/Http/Controllers/GatePassController.php
@@ -2,9 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VisitorFormRequest;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
+use App\Http\Resources\VisitorCollection;
+use App\Http\Resources\VisitorResource;
+use App\Notifications\GatePassRequest;
+use Illuminate\Support\Facades\Notification;
 
 class GatePassController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $visitors =  Visitor::with(['user', 'user.profile'])->estateOnly()->paginate(10);
+
+        return $this->response_data(new VisitorCollection($visitors));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $visitor = Visitor::create([
+            'user_id' => $request->user_id,
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'estate_id' => auth()->user()->estate->random()->id,
+            'sent_by' => User::class,
+            'expired_at' => $request->expired_at ?? now()->addDays(1),
+        ]);
+
+        $visitor->user->notify(new GatePassRequest($visitor));
+
+        return $this->response_success('Created a new visitor');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visitor $visitor)
+    {
+        $visitor->load(['user', 'user.profile']);
+
+        $this->response_data(VisitorResource::make($visitor));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(VisitorFormRequest $request, Visitor $visitor)
+    {
+        $visitor->update($request->validated());
+
+        return $this->response_success('Updated a visitor');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visitor $visitor)
+    {
+        $visitor->delete();
+
+        return $this->response_success('Deleted a visitor');
+    }
 }
